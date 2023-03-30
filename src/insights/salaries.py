700971d3e365ccf962db9acd9a4aa82d918335e3
felipe <felipe_herbert@hotmail.com.br>
2023-03-29 21:11:48 -0300

    requisito 4 concluído

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,25 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
+
+# src.insights.
+path = "data/jobs.csv"
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
+    contents = read(path)
+    all_values = []
+    for content in contents:
+        if content["max_salary"] not in contents:
+            if not content["max_salary"].isalpha():
+                all_values.append(content["max_salary"])
+    all_values_wihout_space = [
+        int(value) for value in all_values if value != ""
+    ]
+    max_number = max(all_values_wihout_space)
+    return max_number
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+get_max_salary(path)
 
 
 def get_min_salary(path: str) -> int:
@@ -64,8 +67,7 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
     """Filters a list of jobs by salary range
 
