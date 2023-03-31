ed5a255a7ea4eaa0f07e3ac220d0213640e7d264
felipe <felipe_herbert@hotmail.com.br>
2023-03-31 11:06:34 -0300

    corrigindo requiito 6

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -26,6 +26,6 @@ def get_unique_job_types(path: str) -> List[str]:
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list_of_jobs = []
     for job in jobs:
-        if job['job_type'] == job_type:
-            list_of_jobs.append(job["job_type"])
+        if job["job_type"] == job_type:
+            list_of_jobs.append(job)
     return list_of_jobs
