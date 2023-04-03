0f6a2f6eb25f9f1d21a06be4c6f78e1d235e149f
felipe <felipe_herbert@hotmail.com.br>
2023-04-03 14:58:29 -0300

    Requisito 11 completo

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
+dic_mock = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file(path)
+    assert result[0] == dic_mock
