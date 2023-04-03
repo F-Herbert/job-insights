1c0b1ff5f691edeef3e10f18a04625bbe5510c9e
felipe <felipe_herbert@hotmail.com.br>
2023-04-03 15:17:14 -0300

    Requisito 12 completo

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,16 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+dic_mock = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+dick_mock2 = {
+    "title": "Analista de Software",
+    "salary": "4000",
+    "type": "full time",
+}
+dick_mock3 = {"title": "Motorista", "salary": "3000", "type": "full time"}
+
+array_of_dict = [dic_mock, dick_mock2, dick_mock3]
 
 
 def test_sort_by_criteria():
-    pass
+    result = sort_by(array_of_dict, "min_salary")
+    assert result == dic_mock
