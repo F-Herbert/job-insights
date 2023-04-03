9f84e7810e6ce69a31988390990593a3bf6a8bad
felipe <felipe_herbert@hotmail.com.br>
2023-04-03 14:15:35 -0300

    Requisito 10 concluído

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+path = "data/jobs.csv"
+word = "Python"
 
 
 def test_counter():
-    pass
+    result = count_ocurrences(path, word)
+    assert result == 1639
